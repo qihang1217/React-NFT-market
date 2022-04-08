e9a0bca39f7ad17c0b2215b90003806490b1fc90
Qihang <1137897907@qq.com>
2022-04-09 05:35:08 +0800

    完成上传界面的各种文件预览功能

--- front-end/src/components/UploadMint/UploadMint.jsx
+++ front-end/src/components/UploadMint/UploadMint.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {InboxOutlined} from '@ant-design/icons';
-import {Button, Form, Input, message, Upload} from 'antd';
+import {Button, Form, Input, message, Modal, Upload} from 'antd';
 import ApiUtil from "../../Utils/ApiUtil";
 
 const {Dragger} = Upload;
@@ -21,6 +21,7 @@ const props = {
     action: 'http://127.0.0.1:5001/api/upload',
     accept: "image/*,video/*,audio/*",
     maxCount: 1,
+    listType:'picture',
     onChange(info) {
         const {status} = info.file;
         if (status !== 'uploading') {
@@ -39,140 +40,225 @@ const props = {
 
 var formData = new FormData();
 
-const normFile = (e) => {
-    let size = e.file.size;
-    let sizeM = size / 1024 / 1024;
-    // console.log(e.file.type)
-    const isSatisfy = (e.file.type.indexOf('image') === 0 || e.file.type.indexOf('video') === 0 || e.file.type.indexOf('audio') === 0);
-    if (!isSatisfy) {
-        message.error(`${e.file.name} 需为图片、视频或音频`);
-        return [];
-    }
-    // console.log(sizeM)
-    const image_limit = 10;
-    if (e.file.type.indexOf('image') === 0) {
-        const isLt = sizeM <= image_limit;
-        if (!isLt) {
-            // setFileLists([]);
-            message.error(`${e.file.name} 图片文件大小不能超过${image_limit}M`);
-            return [];
-        }
-    }
-    const audio_limit = 10;
-    if (e.file.type.indexOf('audio') === 0) {
-        const isLt = sizeM <= audio_limit;
-        if (!isLt) {
-            // setFileLists([]);
-            message.error(`${e.file.name} 音频文件大小不能超过${audio_limit}M`);
-            return [];
-        }
-    }
-    const video_limit = 100;
-    if (e.file.type.indexOf('video') === 0) {
-        const isLt = sizeM <= video_limit;
-        if (!isLt) {
-            // setFileLists([]);
-            message.error(`${e.file.name} 视频文件大小不能超过${video_limit}M`);
-            return [];
-        }
-    }
-    formData.append('file', e.file)
-    // console.log('UploadMint event:', e);
-    return e && e.fileList;
-};
 
-/*todo:添加上传文件根据文件类型进行预览
-https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=monline_4_dg&wd=antd%20%E9%A2%84%E8%A7%88%E8%A7%86%E9%A2%91&oq=antd%2520%25E8%2587%25AA%25E5%25AE%259A%25E4%25B9%2589%25E9%25A2%2584%25E8%25A7%2588%25E8%25A7%2586%25E9%25A2%2591&rsv_pq=be4df6ef0038692f&rsv_t=b75foeWyNzqw1JsrhOYyQWQ2XBZlI2HXMsS7lK0xNz0W2BceVYTo%2FO1%2BnJwFxWv5PzR6&rqlang=cn&rsv_dl=tb&rsv_enter=1&rsv_btype=t&inputT=924&rsv_sug3=53&rsv_sug1=52&rsv_sug7=100&rsv_sug2=0&rsv_sug4=1792
-https://cloud.tencent.com/developer/ask/sof/1152199*/
-const onSubmit = (values) => {
-    // console.log(values)
-    let token = localStorage.getItem('token')
-    formData.append('token', token)
-    formData.append('work_name', values.work_name)
-    formData.append('introduction', values.introduction)
-    fetch(ApiUtil.API_UPLOAD, {
-        //fetch请求
-        method: 'POST',
-        body: formData
-    }).then(response => response.json())
-        .then(result => {
-            console.log(result)
-            if (result.responseCode === 200 && result.message === '上传成功') {
-                message.success('NTF铸造信息提交成功,正在火速为您审核中~');
-            } else if (result.responseCode === -1 && result.token_message === '未登录') {
-                message.error('登陆状态无效或未登录,请重新登陆~');
-                //清除存储的无效登陆信息
-                localStorage.removeItem('token')
-                localStorage.removeItem('user_name')
-                window.location.href = '/login'
-            } else if (result.responseCode === -1 && result.detail_message === '文件类型不合格') {
-                message.error('作品格式不符合要求,请重新上传作品~');
-            } else {
-                message.error('NTF铸造信息提交失败,请重新提交~');
-            }
-        })
-        .catch(function (error) {
-            // console.log(error);
+class UploadMint extends React.Component{
+    state = {
+        previewVisible: false,
+        previewTitle: '',
+        previewContent: null,
+        data: null,
+        fileList:[],
+    }
+
+    handlePreview=(file)=>{
+        console.log(file)
+        this.setState({
+            previewVisible: true,
+            previewTitle: file.name,
         });
-};
+    }
+
+    handleOk = () => this.setState({ previewVisible: false });
+
+    handleCancel = () => {
+        this.setState({ previewVisible: false })
+        let delete_button = document.getElementsByClassName("ant-btn ant-btn-text ant-btn-sm ant-btn-icon-only ant-upload-list-item-card-actions-btn")[0];
+        console.log(delete_button)
+        delete_button.click();
+    }
 
 
-const UploadMint = () => {
-    return (
-        <div>
-            <div className="card mt-1">
-                <div className="card-body align-items-center d-flex justify-content-center">
-                    <h5>
-                        上传你的作品，让你的作品独一无二！
-                    </h5>
+    onSubmit = (values) => {
+        // console.log(values)
+        let token = localStorage.getItem('token')
+        formData.append('token', token)
+        formData.append('work_name', values.work_name)
+        formData.append('introduction', values.introduction)
+        fetch(ApiUtil.API_UPLOAD, {
+            //fetch请求
+            method: 'POST',
+            body: formData
+        }).then(response => response.json())
+            .then(result => {
+                console.log(result)
+                if (result.responseCode === 200 && result.message === '上传成功') {
+                    message.success('NTF铸造信息提交成功,正在火速为您审核中~');
+                } else if (result.responseCode === -1 && result.token_message === '未登录') {
+                    message.error('登陆状态无效或未登录,请重新登陆~');
+                    //清除存储的无效登陆信息
+                    localStorage.removeItem('token')
+                    localStorage.removeItem('user_name')
+                    window.location.href = '/login'
+                } else if (result.responseCode === -1 && result.detail_message === '文件类型不合格') {
+                    message.error('作品格式不符合要求,请重新上传作品~');
+                } else {
+                    message.error('NTF铸造信息提交失败,请重新提交~');
+                }
+            })
+            .catch(function (error) {
+                // console.log(error);
+            });
+    };
+
+
+    normFile = (e) => {
+        console.log(e)
+        let file=e.file
+        if(file.originFileObj){
+            //删除时
+            file=file.originFileObj
+        }
+        else
+        {
+            let size = file.size;
+            let sizeM = size / 1024 / 1024;
+            // console.log(file.type)
+            let src,previewContent,type=file.type;
+            const isSatisfy = /^image\/\S+$/.test(type) || /^video\/\S+$/.test(type) || /^audio\/\S+$/.test(type)||/^text\/\S+$/.test(type);
+            if (!isSatisfy) {
+                message.error(`${file.name} 需为图片、视频、音频或文本`);
+                return [];
+            }
+
+            // console.log(sizeM)
+            const image_limit = 10;
+            const audio_limit = 10;
+            const video_limit = 100;
+            const text_limit = 10;
+            if (/^image\/\S+$/.test(type)) {
+                const isLt = sizeM <= image_limit;
+                if (!isLt) {
+                    message.error(`${file.name} 图片文件大小不能超过${image_limit}M`);
+                    return [];
+                }else{
+                    // 获取当前文件的一个内存URL
+                    src = URL.createObjectURL(file)
+                    previewContent = <img src={src} alt='上传的图片' />
+                }
+            }else if (/^video\/\S+$/.test(type)) {
+                const isLt = sizeM <= video_limit;
+                if (!isLt) {
+                    message.error(`${file.name} 视频文件大小不能超过${audio_limit}M`);
+                    return [];
+                }else{
+                    src = URL.createObjectURL(file)
+                    previewContent = <video src={src} autoPlay loop controls />
+                }
+            }else if (/^audio\/\S+$/.test(type)) {
+                const isLt = sizeM <= audio_limit;
+                if (!isLt) {
+                    message.error(`${file.name} 音频文件大小不能超过${video_limit}M`);
+                    return [];
+                }else{
+                    src = URL.createObjectURL(file)
+                    previewContent = (
+                        <audio controls autoPlay>
+                            <source src={src}/>
+                            <embed src={src}/>
+                        </audio>
+                    )
+                }
+            }else if (/^text\/\S+$/.test(type)) {
+                const isLt = sizeM <= text_limit;
+                if (!isLt) {
+                    message.error(`${file.name} 文本文件大小不能超过${video_limit}M`);
+                    return [];
+                }else{
+                    const self = this;
+                    const reader = new FileReader();
+                    reader.readAsText(file);
+                    //注：onload是异步函数，此处需独立处理
+                    reader.onload = function (e) {
+                        previewContent = <textarea value={this.result} readOnly/>
+                        self.setState({ previewVisible:true,previewTitle: file.name, data: file, previewContent: previewContent })
+                    }
+                    return;
+                }
+
+            }
+            this.setState({ previewVisible:true,previewTitle: file.name, data: file, previewContent: previewContent })
+            //确保传输的文件只有一个
+            formData=new FormData()
+            formData.append('file', file)
+            console.log(formData)
+            // console.log('UploadMint event:', e);
+            return e && e.fileList;
+        }
+
+    };
+
+
+    render() {
+        const {previewContent ,previewVisible,previewTitle} = this.state;
+        return (
+            <div>
+                <div className="card mt-1">
+                    <div className="card-body align-items-center d-flex justify-content-center">
+                        <h5>
+                            上传你的作品，让你的作品独一无二！
+                        </h5>
+                    </div>
                 </div>
-            </div>
-            <Form {...layout} name="nest-messages" onFinish={onSubmit}>
-                <Form.Item
-                    name="upload-file"
-                    label='文件上传'
-                    valuePropName="fileList"
-                    getValueFromEvent={normFile}
-                    rules={[
-                        {
-                            required: true,
-                            message: '请上传您的作品!'
-                        },
-                    ]}>
-                    <Dragger {...props} >
-                        <p className="ant-upload-drag-icon">
-                            <InboxOutlined/>
-                        </p>
-                        <p className="ant-upload-text">单击或拖动文件到此区域以上载</p>
-                        <p className="ant-upload-hint">
-                            支持单次或批量上传,仅支持图片、音频或视频
-                        </p>
-                    </Dragger>
-                </Form.Item>
-                <Form.Item
-                    name="work_name"
-                    label="作品名称"
-                    rules={[
-                        {
-                            required: true,
-                            message: '请输入您的作品名称!'
-                        },
-                    ]}
+                <Form {...layout} name="nest-messages" onFinish={this.onSubmit}>
+                    <Form.Item
+                        name="upload-file"
+                        label='文件上传'
+                        valuePropName="fileList"
+                        getValueFromEvent={this.normFile}
+                        rules={[
+                            {
+                                required: true,
+                                message: '请上传您的作品!'
+                            },
+                        ]}>
+                        <Dragger
+                            {...props} fileList={this.state.fileList}
+                        >
+                            <p className="ant-upload-drag-icon">
+                                <InboxOutlined/>
+                            </p>
+                            <p className="ant-upload-text">单击或拖动文件到此区域以上传</p>
+                            <p className="ant-upload-hint">
+                                支持单次或批量上传,仅支持图片、音频或视频
+                            </p>
+                        </Dragger>
+                    </Form.Item>
+                    <Form.Item
+                        name="work_name"
+                        label="作品名称"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入您的作品名称!'
+                            },
+                        ]}
+                    >
+                        <Input maxlength="20" placeholder="作品名称"/>
+                    </Form.Item>
+                    <Form.Item name='introduction' label="作品介绍">
+                        <Input.TextArea maxlength="1000" placeholder="请输入您的作品描述"/>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{...layout.wrapperCol, offset: 6}}>
+                        <Button type="primary" htmlType="submit">
+                            提交
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    okText='确定使用'
+                    cancelText='换一个'
+                    onCancel={this.handleCancel}
+                    onOk={this.handleOk}
                 >
-                    <Input maxlength="20" placeholder="作品名称"/>
-                </Form.Item>
-                <Form.Item name='introduction' label="作品介绍">
-                    <Input.TextArea maxlength="1000" placeholder="请输入您的作品描述"/>
-                </Form.Item>
-                <Form.Item wrapperCol={{...layout.wrapperCol, offset: 6}}>
-                    <Button type="primary" htmlType="submit">
-                        提交
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
+                    {previewContent}
+                </Modal>
 
-    );
+            </div>
+
+        );
+    }
 
 }
 
