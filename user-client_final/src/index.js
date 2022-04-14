d935012ee293d719cc181f63b65a79e8af72baea
Qihang <1137897907@qq.com>
2022-04-15 02:33:27 +0800

    设置antd的默认语言为中文

--- user-client_final/src/index.js
+++ user-client_final/src/index.js
@@ -6,16 +6,19 @@ import {BrowserRouter as Router, Switch} from "react-router-dom";
 import routerMap from "./router/routerMap.js"
 import ReactDOM from "react-dom";
 import FrontendAuth from "./router/FrontendAuth";
-
+import {ConfigProvider} from "antd";
+import zhCN from "antd/lib/locale/zh_CN"
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Switch>
-                {/*利用高阶组件进行路由有效性检验和响应跳转*/}
-                <FrontendAuth routerConfig={routerMap}/>
-            </Switch>
-        </Router>
-    </React.StrictMode>,
+    <ConfigProvider locale={zhCN}>
+        <React.StrictMode>
+            <Router>
+                <Switch>
+                    {/*利用高阶组件进行路由有效性检验和响应跳转*/}
+                    <FrontendAuth routerConfig={routerMap}/>
+                </Switch>
+            </Router>
+        </React.StrictMode>
+    </ConfigProvider>,
     document.getElementById("root")
 );
